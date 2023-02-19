cf8c99a60586b552ada7c331cd994e0f6442fab8
pratik <pratikofficial26@gmail.com>
2023-02-19 18:59:06 +0530

    inputs

--- 2048.clj
+++ 2048.clj
@@ -1,8 +1,8 @@
 
-(def textures {:wall      "----+"
+(def layout {  :boundary      "----+"
                :cell      "%4s|"
-               :cell-edge "|"
-               :wall-edge "+"})
+               :cellEdge "|"
+               :boundaryEdge "+"})
 
 (def directions {:w :up
                  :s :down
@@ -15,21 +15,56 @@
 
 ;cells design 
 (defn cells->str [line]                    
-  (str (:cell-edge textures)
-       (clojure.string/join (map (partial format (:cell textures)) line))
+  (str (:cellEdge layout)
+       (clojure.string/join (map (partial format (:cell layout)) line))
        "\n"))
 
-; wall-edge + and walls design
-(defn walls->str [width]
-  (str (:wall-edge textures)
-       (clojure.string/join (repeat width (:wall textures)))
+; boundary-edge + and boundary design
+(defn boundaries->str [width]
+  (str (:boundaryEdge layout)
+       (clojure.string/join (repeat width (:boundary layout)))
        "\n"))
 
-
+;merging bounaries and cells
 (defn field->str [field]
   (let [height (count field)
         width (count (first field))]
-    (str (clojure.string/join (interleave (repeat height (walls->str width))
+    (str (clojure.string/join (interleave (repeat height (boundaries->str width))
                                (map cells->str field)))
-         (walls->str width))))
+         (boundaries->str width))))
 
+
+
+;input of the keys
+
+(defn inputKeys []
+  (let [input (read)
+        try ((keyword input) directions)]
+    (if try try (recur))))
+
+(defn getColumns [field]
+  (vec (for [x (range (count (first field)))]
+         (vec (for [y (range (count field))]
+                (get-in field [y x]))))))
+
+(defn reverseRow [field]
+  (mapv #(vec (reverse %)) field))
+
+(defn padding [coll n sym]
+  (vec (concat coll (repeat n sym))))
+
+(defn emptyField [field]
+  (remove
+    nil?
+    (for [y (range (count field))
+          x (range (count (nth field y)))]
+      (when (= (get-in field [y x]) \space) [y x]))))
+
+(defn add [field]
+  (let [empties (vec (emptyField field))]
+    (assoc-in field
+              (rand-nth empties)
+              (rand-nth (conj (vec (repeat 9 2)) 4))))) 
+
+
+; win and lose logic
