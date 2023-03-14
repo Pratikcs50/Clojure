08ec79f517af242c3dcf1a536e43b5141759c7cc
pratik <pratikofficial26@gmail.com>
2023-03-14 13:23:31 +0530

    Macros-koans

---
+++ macros.clj
@@ -0,0 +1,42 @@
+(defmacro hello [x]
+  (str "Hello, " x))
+
+(defmacro infix [form]
+  (list (second form) (first form) (nth form 2)))
+
+(defmacro infix-concise [form]
+  `(~(second form) ; Note the syntax-quote (`) and unquote (~) characters!
+    ~(first form)
+    ~(nth form 2)))
+
+(defmacro recursive-infix [form]
+  (cond (not (seq? form))
+        form
+        (= 1 (count form))
+        `(recursive-infix ~(first form))
+        :else
+        (let [operator (second form)
+              first-arg (first form)
+              others (rest (rest form))]
+          `(~operator
+            (recursive-infix ~first-arg)
+            (recursive-infix ~others)))))
+
+
+; "Macros are like functions created at compile time"
+ (println (= "Hello, Macros!" (hello "Macros!"))) 
+
+;   "I can haz infix?"
+ (println (= 10 (infix (9 + 1)))) 
+
+;   "Remember, these are nothing but code transformations"
+(println (= '(+ 9 1) (macroexpand '(infix (9 + 1)))))  
+
+;   "You can do better than that - hand crafting FTW!"
+(println (= '(* 10 2) (macroexpand '(infix-concise (10 * 2)))))  
+ 
+;   "Things don't always work as you would like them to"
+(println (= '(+ 10 (2 * 3)) (macroexpand '(infix-concise (10 + (2 * 3))))))  
+
+;   "Really, you don't understand recursion until you understand recursion"
+(println (= 36 (recursive-infix (10 + (2 * 3) + (4 * 5)))))  
